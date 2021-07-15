ee9788f3586f1f2f5446657b86ec79e2da34727d
Ricardo-Gomes <rikardos38@gmail.com>
2021-07-15 18:15:29 -0300

    5.4. Prototipando o cadastro da cerveja

--- brewer/src/main/java/com/algaworks/brewer/model/Cerveja.java
+++ brewer/src/main/java/com/algaworks/brewer/model/Cerveja.java
@@ -6,14 +6,14 @@ import org.hibernate.validator.constraints.NotBlank;
 
 public class Cerveja {
 	
-	@NotBlank
+	@NotBlank(message = "SKU é obrigatório")
 	private String sku;
 	
-	@NotBlank
+	@NotBlank(message = "Nome é obrigatório")
 	private String nome;
 	
 	@NotBlank
-	@Size(max = 50)
+	@Size(min = 1, max = 50, message = "O tamanho da descrição deve estar entre 1 a 50 caracteres")
 	private String descricao;
 	
 	public String getSku() {
